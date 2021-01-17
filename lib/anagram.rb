845ff205c0a5099f65949c559fdca85049e186ed
Kevin Jung <kev.jung.ii@gmail.com>
2021-01-17 18:27:20 -0500

    commit tree 1c761996341c8c58521ef5304125977839edfb42

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :letters, :word
+  def initialize(word)
+    @word = word
+    @letters = word.scan(/\w/).sort
+  end
+  def match(words)
+    matches = []
+    words.select { |word| @letters.length == word.length}.collect { |word| word.scan(/\w/) }.each { |word| matches << word.join if word.sort == @letters }
+    matches
+  end
+end
